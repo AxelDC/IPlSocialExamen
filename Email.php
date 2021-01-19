3867e33326189fdc5f49494bb87ba568354cfae1
axel <axel.declerck@student.vinci.be>
2021-01-19 11:11:33 +0100

    Test true implement method

--- Email.php
+++ Email.php
@@ -4,5 +4,12 @@
 class Email
 {
 
+    public function checkEmail(string $str):bool{
+        $reg="/^([a-zA-Z0-9-.]+)@([a-zA-Z0-9-.]+).([a-zA-Z]{2,5})$/";
+        if(preg_match($reg,$str)) return true;
 
+        return false;
+
+
+    }
 }
--- EmailTest.php
+++ EmailTest.php
@@ -1,11 +1,17 @@
 <?php
 
+require "Email.php";
 
 class EmailTest extends PHPUnit_Framework_TestCase
 {
 
     public function test_checkEmail_givenGoodEmail_shouldReturnTrue(){
-        $this->assertEquals(true,true);
+        $email = new Email();
+
+        $test = $email->checkEmail("axel.dec@hotmail.com");
+
+
+        $this->assertEquals(true,$test);
     }
 
     public function test_checkEmail_givenBadEmail_shouldReturnFalse(){
